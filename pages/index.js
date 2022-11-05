b780d4445904d0f3dae4a40a006a178d53afe099
jamacio.rocha <jamacio.rocha@danone.com>
2022-11-04 21:57:33 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 
 const Minio = require('minio')
 
-const uploadImageS3 = async (reader, file) => {
-  const fileData = reader.replace(/^data:image\/\w+;base64,/, "");
-  const buffer = Buffer.from(fileData, 'base64');
-
-  const minioClient = new Minio.Client({
-    endPoint: '191.101.234.188',
-    port: 9000,
-    useSSL: false,
-    accessKey: 'minio',
-    secretKey: 'minio123'
-  });
-
-  minioClient.putObject('menu', file.name, buffer, function (err, etag) {
-    return console.log(err, etag)
-  })
-}
-
 export default function Home() {
   const [imgsSrc, setImgsSrc] = useState([]);
+  const [imgName, setImgName] = useState("");
+
+  const uploadImageS3 = async (reader, file) => {
+    const fileData = reader.replace(/^data:image\/\w+;base64,/, "");
+    const buffer = Buffer.from(fileData, 'base64');
+
+    const minioClient = new Minio.Client({
+      endPoint: '191.101.234.188',
+      port: 9000,
+      useSSL: false,
+      accessKey: 'minio',
+      secretKey: 'minio123'
+    });
+
+    setImgName(file.name)
+
+    minioClient.putObject('menu', file.name, buffer, function (err, etag) {
+      return console.log(err, etag)
+    })
+  }
+
   const onChange = (e) => {
     for (const file of e.target.files) {
       const reader = new FileReader();
@@ -42,6 +43,7 @@ export default function Home() {
   return (
     <div>
       <input onChange={onChange} type="file" name="file" />
+      {imgName}
       {imgsSrc.map((link, key) => (
         <img key={key} src={link} />
       ))}
