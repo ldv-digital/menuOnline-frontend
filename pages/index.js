abaa61a8fb9f152303a0e4c365ce4672bb97786b
jamacio.rocha <jamacio.rocha@danone.com>
2022-12-11 14:33:44 -0300

    sanatize image name

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,19 @@
 import { useState } from 'react'
-
-const Minio = require('minio')
+import uploadImageS3 from '../Minio'
 
 export default function Home() {
   const [imgsSrc, setImgsSrc] = useState([]);
   const [imgName, setImgName] = useState("");
-
-  const uploadImageS3 = async (reader, file) => {
-    const fileData = reader.replace(/^data:image\/\w+;base64,/, "");
-    const buffer = Buffer.from(fileData, 'base64');
-
-    const minioClient = new Minio.Client({
-      endPoint: '191.101.234.188',
-      port: 9000,
-      useSSL: false,
-      accessKey: 'minio',
-      secretKey: 'minio123'
-    });
-
-    setImgName(file.name)
-
-    minioClient.putObject('menu', file.name, buffer, function (err, etag) {
-      return console.log(err, etag)
-    })
-  }
 
   const onChange = (e) => {
     for (const file of e.target.files) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
-        uploadImageS3(reader.result, file);
+        uploadImageS3(reader.result, file).then(function (result) {
+          setImgName(result.fileName)
+        });
+
         setImgsSrc((imgs) => [...imgs, reader.result]);
       };
 
