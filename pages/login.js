57a115ccfbfc20ef3181194b554cca31c310c72d
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-01-30 15:32:57 -0300

    add new layout on page login and add login dinamic

--- pages/login.js
+++ pages/login.js
@@ -1,65 +1,91 @@
-import { gql } from "@apollo/client";
-import client from "../services/apollo-client";
+import { gql } from '@apollo/client'
+import { useState } from 'react'
+import client from '../services/apollo-client'
 import styles from '../styles/Home.module.css'
 export default function Login() {
-    // makeLogin("jamacio@hotmail.com", "teste@123456")
+  const [getUser, setUser] = useState('')
+  const [getPass, setPass] = useState('')
 
-    return (
-      <div className={styles.body}>
-        <div className={styles.login_body}>
-          <div className={styles.login_box}>
-            <h2>Menu Online</h2>
-              <form>
-                <div className={styles.input_box}>
-                    <input required type="email" placeholder="Email"/>
-                </div>
+  function handleLogin() {
+    makeLogin(getUser, getPass)
+  }
 
-                <div className={styles.input_box}>
-                    <input required type="password" placeholder="Senha"/>
-                </div>
+  function handleUser(event) {
+    setUser(event.target.value)
+  }
 
-                <div>
-                    <button className={styles.submit}>
-                        Login
-                    </button>
-                </div>
-              </form>
+  function handlePass(event) {
+    setPass(event.target.value)
+  }
 
-              <div className={styles.support}>
-                <div className={styles.remember}>
-                    <span><input type="checkbox"/></span>
-                    <span>Lembre-se de mim</span>
-                </div>
-                <div className={styles.help}>
-                    <a href="#">
-                        Precisa de ajuda?
-                    </a>
-                </div>
-              </div>
+  return (
+    <div className={styles.body}>
+      <div className={styles.login_body}>
+        <div className={styles.login_box}>
+          <h2>Menu Online</h2>
+          <form>
+            <div className={styles.input_box}>
+              <input
+                required
+                type="email"
+                placeholder="Email"
+                onChange={handleUser}
+              />
+            </div>
 
-              <div className={styles.login_footer}>
-                
-                <div className={styles.sign_up}>
-                  <p>Novo por aqui? <a href="#">Cadastre-se.</a></p>
-                </div>
+            <div className={styles.input_box}>
+              <input
+                required
+                type="password"
+                placeholder="Senha"
+                onChange={handlePass}
+              />
+            </div>
 
-                <div className={styles.terms}>
-                  <p>Esta página é a pagina de acesso ao site Menu Online.<br/> Para mais informações clique em: <b/>
-                  <a href="#">Saiba mais</a></p>
-                </div>
-              </div>
+            <div>
+              <button onClick={handleLogin} className={styles.submit}>
+                Login
+              </button>
+            </div>
+          </form>
+
+          <div className={styles.support}>
+            <div className={styles.remember}>
+              <span>
+                <input type="checkbox" />
+              </span>
+              <span>Lembre-se de mim</span>
+            </div>
+            <div className={styles.help}>
+              <a href="#">Precisa de ajuda?</a>
+            </div>
+          </div>
+
+          <div className={styles.login_footer}>
+            <div className={styles.sign_up}>
+              <p>
+                Novo por aqui? <a href="#">Cadastre-se.</a>
+              </p>
+            </div>
+
+            <div className={styles.terms}>
+              <p>
+                Esta página é a pagina de acesso ao site Menu Online.
+                <br /> Para mais informações clique em: <b />
+                <a href="#">Saiba mais</a>
+              </p>
+            </div>
           </div>
         </div>
-
       </div>
-    );
+    </div>
+  )
 }
 
 async function makeLogin(user, pass) {
-
-    const result = await client.mutate({
-        mutation: gql`
-      mutation LoginUser ($user: String, $pass: String) {
+  const result = await client.mutate({
+    mutation: gql`
+      mutation LoginUser($user: String, $pass: String) {
         getLogin(email: $user, pass: $pass) {
           token
           email
@@ -67,9 +93,8 @@ async function makeLogin(user, pass) {
         }
       }
     `,
-        variables: { user, pass }
-    })
+    variables: { user, pass }
+  })
 
-    localStorage.setItem('token', result?.data?.getLogin?.token);
+  localStorage.setItem('token', result?.data?.getLogin?.token)
 }
-
