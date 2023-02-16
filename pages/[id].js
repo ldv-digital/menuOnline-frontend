19a839bf9c7a009f463334266f36dc2b88767074
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-02-16 14:54:06 -0300

    add style for input error

--- pages/[id].js
+++ pages/[id].js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react'
-import { gql } from "@apollo/client";
-import client from "../services/apollo-client";
+import { gql } from '@apollo/client'
+import client from '../services/apollo-client'
 import { useRouter } from 'next/router'
 
 export default function MenuCliente() {
-  const urlMinio = '//' + process.env.NEXT_PUBLIC_ENDPOINT + ':' + process.env.NEXT_PUBLIC_PORT + '/' + process.env.NEXT_PUBLIC_BUCKET;
-  const [urlMenu, setUrlMenu] = useState('');
+  const urlMinio =
+    '//' +
+    process.env.NEXT_PUBLIC_ENDPOINT +
+    ':' +
+    process.env.NEXT_PUBLIC_PORT +
+    '/' +
+    process.env.NEXT_PUBLIC_BUCKET
+  const [urlMenu, setUrlMenu] = useState('')
   const router = useRouter()
   const { id } = router.query
 
   useEffect(() => {
     if (id) {
-      getMenuById(id).then((value) => {
-        const { urlMenu } = value;
-        setUrlMenu(urlMinio+'/'+urlMenu)
-
+      getMenuById(id).then(value => {
+        const { urlMenu } = value
+        setUrlMenu(urlMinio + '/' + urlMenu)
       })
     }
   }, [id])
@@ -23,11 +28,10 @@ export default function MenuCliente() {
     <div>
       <img src={urlMenu} />
     </div>
-  );
+  )
 }
 
 async function getMenuById(id_menu) {
-
   const { data } = await client.query({
     query: gql`
       query Menu($id_menu: ID) {
@@ -39,8 +43,7 @@ async function getMenuById(id_menu) {
       }
     `,
     variables: { id_menu }
-  });
+  })
 
-  return data?.getMenu;
+  return data?.getMenu
 }
-
