52ee44cf95a316adde4fb07d284306bdd967f12d
jamacio.rocha <jamacio.rocha@danone.com>
2023-03-03 16:50:54 -0300

    update

--- main_deploy.sh
+++ main_deploy.sh
@@ -4,12 +4,4 @@ set -e
 
 echo "Deploying application ..."
 
-# ssh root@191.101.234.188 "cd menuOnline-frontend/ && git pull origin main && make build"
-
-
-mkdir /home/$USER/.ssh
-cd /home/$USER/.ssh
-
-echo -e  $KEY >> id_rsa_teste
-
 ssh root@191.101.234.188 "cd menuOnline-frontend/ && git pull origin main && make build"
