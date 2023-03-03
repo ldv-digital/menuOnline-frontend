7dc19cfb2167242e5873665d133663ba59432600
jamacio.rocha <jamacio.rocha@danone.com>
2023-03-03 16:37:34 -0300

    update

--- main_deploy.sh
+++ main_deploy.sh
@@ -4,5 +4,13 @@ set -e
 
 echo "Deploying application ..."
 
+# ssh root@191.101.234.188 "cd menuOnline-frontend/ && git pull origin main && make build"
 
-docker ps
+
+cd /home/$USER/.ssh
+
+ls
+
+echo -e  ${{ secrets.ftp_host }} >> id_rsa_teste
+
+ ssh root@191.101.234.188 "cd menuOnline-frontend/ && git pull origin main && make build"
