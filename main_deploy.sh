1de704c21a08e018d5375b4798566a0b1855c987
jamacio.rocha <jamacio.rocha@danone.com>
2023-03-03 17:27:23 -0300

    update

--- main_deploy.sh
+++ main_deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "Deploying application ..."
 
-ssh root@191.101.234.188 "cd menuOnline-frontend/ && git pull origin main && make build"
+ssh -t root@191.101.234.188 "cd menuOnline-frontend/ && git pull origin main && make build"
