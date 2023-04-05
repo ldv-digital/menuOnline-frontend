7fc1d4c4d2a013f6783d405bd86d32391ae026be
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-04-05 12:52:49 -0300

    add content to home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { Navigation } from '../components/Navigation'
+import Image from 'next/image'
 import styles from './index.module.css'
+import img from '../public/img.jpeg'
 import { Footer } from '../components/footer'
 
 export default function Home() {
@@ -7,7 +9,24 @@ export default function Home() {
     <>
       <Navigation />
       <div className={styles.body}>
-        <h1>Bem vindo!!!</h1>
+
+        <div className={styles.home}>
+        
+        <h2 className={styles.animeLeft}>
+        " Seja {'bem-vindo(a)'} ao Menu Online! Aqui você encontrará uma maneira fácil 
+         e prática para que os donos de bares e restaurantes possam hospedar uma 
+         imagem do cardápio online. Com o nosso serviço, você poderá compartilhar 
+         o seu cardápio com seus clientes de forma rápida e eficiente, evitando a 
+         impressão de cardápios físicos e ajudando na preservação do meio ambiente. 
+         Navegue pelo nosso site e descubra como é simples ter o seu cardápio online. 
+         Cadastre-se agora e comece a aproveitar todas as vantagens que oferecemos 
+         para o seu negócio! "
+        </h2>
+        <div className={`${styles.imagens} ${styles.animeRight}`}>
+          <Image src={img} alt="imagem ilustrativa de um cardapio"/>
+        </div>
+
+        </div>
       </div>
       <Footer />
     </>
