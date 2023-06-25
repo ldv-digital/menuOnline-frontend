5786a88653955a3f03ea67d6e2c5ab6668c4f163
Jamacio <jamacio.rocha@danone.com>
2023-06-25 02:45:15 -0300

    test pipelines

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,20 @@ export default function Home() {
       <div className={styles.body}>
 
         <div className={styles.home}>
-        
-        <h2 className={styles.animeLeft}>
-        " Seja {'bem-vindo(a)'} ao Menu Online! Aqui você encontrará uma maneira fácil 
-         e prática para que os donos de bares e restaurantes possam hospedar uma 
-         imagem do cardápio online. Com o nosso serviço, você poderá compartilhar 
-         o seu cardápio com seus clientes de forma rápida e eficiente, evitando a 
-         impressão de cardápios físicos e ajudando na preservação do meio ambiente. 
-         Navegue pelo nosso site e descubra como é simples ter o seu cardápio online. 
-         Cadastre-se agora e comece a aproveitar todas as vantagens que oferecemos 
-         para o seu negócio! "
-        </h2>
-        <div className={`${styles.imagens} ${styles.animeRight}`}>
-          <Image src={img} alt="imagem ilustrativa de um cardapio"/>
-        </div>
+
+          <h2 className={styles.animeLeft}>
+            "teste Seja {'bem-vindo(a)'} ao Menu Online! Aqui você encontrará uma maneira fácil
+            e prática para que os donos de bares e restaurantes possam hospedar uma
+            imagem do cardápio online. Com o nosso serviço, você poderá compartilhar
+            o seu cardápio com seus clientes de forma rápida e eficiente, evitando a
+            impressão de cardápios físicos e ajudando na preservação do meio ambiente.
+            Navegue pelo nosso site e descubra como é simples ter o seu cardápio online.
+            Cadastre-se agora e comece a aproveitar todas as vantagens que oferecemos
+            para o seu negócio! "
+          </h2>
+          <div className={`${styles.imagens} ${styles.animeRight}`}>
+            <Image src={img} alt="imagem ilustrativa de um cardapio" />
+          </div>
 
         </div>
       </div>
